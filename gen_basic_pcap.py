5adbc47039e3c9d62ba9f95d1b689bc13ea93354
Ethan <etgold05@gmail.com>
2025-03-18 17:33:15 +0200

    change storing

--- gen_basic_pcap.py
+++ gen_basic_pcap.py
@@ -1,3 +1,7 @@
+import Crypto.Cipher.AES as AES
+import hmac
+import hashlib
+import binascii
 from scapy.all import *
 from scapy.layers.l2 import ARP, Ether, LLC, SNAP
 
@@ -21,11 +25,10 @@ PCAP_LOC = "assets/template.pcap"
 
 ALICE_IP = "196.162.0.5"
 BOB_IP = "196.162.0.9"
-ALICE_MAC = "AA:AA:AA:AA:AA:AA"
-BOB_MAC = "BB:BB:BB:BB:BB:BB"
+ALICE_MAC = "aa:aa:aa:aa:aa:aa"
+BOB_MAC = "bb:bb:bb:bb:bb:bb"
 
 bssid = "be:09:77:06:31:96"
-client_mac = "24:16:1b:ca:e0:c0"
 anonce = b"\xaa" * 32  # Example ANonce
 snonce = b"\x11" * 32  # Example SNonce
 ptk = b"\x33" * 48  # Example PTK
@@ -48,6 +51,112 @@ def encrypt_packet(
     anonce, snonce, src_mac, dst_mac, ssid, passkey, data, header
 ) -> Packet:
     pass
+
+
+def get_key_data(ap_mac: bytes, cl_mac: bytes, anonce: bytes, snonce: bytes) -> bytes:
+    mac_addresses_pair = b"".join(sorted([ap_mac, cl_mac]))
+    nonce_pair = b"".join(sorted([anonce, snonce]))
+    key_data = mac_addresses_pair + nonce_pair
+    return key_data
+
+
+def mac_string_to_bytes(mac_string: str) -> bytes:
+    return binascii.unhexlify(mac_string.replace(":", "", 5))
+
+
+def construct_aad(frame):
+    data = bytes(frame)
+    return data[0:2] + data[4:22] + data[30:34]
+
+
+def encrypt_packet(
+    src_mac: str,
+    dst_mac: str,
+    anonce: bytes,
+    snonce: bytes,
+    ssid: str,
+    passkey: str,
+    header,
+    payload,
+):
+    header_bytes = bytes(header)
+    assert len(header_bytes) >= 34
+    pmk = calc_pmk(ssid.encode("ascii"), passkey.encode("ascii"))
+    key_data = get_key_data(
+        mac_string_to_bytes(src_mac), mac_string_to_bytes(dst_mac), anonce, snonce
+    )
+    ptk = calc_ptk(pmk, key_data)
+    tk = ptk[32:48]
+    aad = construct_aad(header)
+    ccmp_header = header[Dot11CCMP]
+    packet_number = b"".join(
+        [
+            i.to_bytes(1, "little")
+            for i in [
+                ccmp_header.PN0,
+                ccmp_header.PN1,
+                ccmp_header.PN2,
+                ccmp_header.PN3,
+                ccmp_header.PN4,
+                ccmp_header.PN5,
+            ]
+        ]
+    )
+    plaintext = bytes(payload)
+    ccm_nonce = (
+        struct.pack(">B", 0) + mac_string_to_bytes(header.addr2) + packet_number[::-1]
+    )
+    aes_ccm = AES.new(tk, AES.MODE_CCM, nonce=ccm_nonce, mac_len=8)
+    aes_ccm.update(aad)
+    ciphertext, mic = aes_ccm.encrypt_and_digest(plaintext)
+    data_frame = header
+    data_frame[Dot11CCMP].data = ciphertext + mic
+    return data_frame
+
+
+def calc_mic(ptk: bytes, mic_frame: bytes) -> bytes:
+    mic = hmac.new(ptk[0:16], mic_frame, "sha1").digest()
+    return mic[:-4]
+
+
+def calc_ptk(pmk: bytes, key_data: bytes) -> bytes:
+    pke = b"Pairwise key expansion"
+    blen = 64
+    i = 0
+    ptk = b""
+
+    while len(ptk) < blen:
+        hmacsha1 = hmac.new(pmk, pke + b"\x00" + key_data + bytes([i]), hashlib.sha1)
+        ptk += hmacsha1.digest()
+        i += 1
+
+    return ptk[:blen]
+
+
+def calc_pmk(ssid: bytes, password: bytes) -> bytes:
+    pmk = hashlib.pbkdf2_hmac("sha1", password, ssid, 4096, 32)
+    return pmk
+
+
+def gen_header(packet_number: int, src_mac: str, dst_mac: str, bssid: str):
+    pn = packet_number.to_bytes(6, "little")
+    return (
+        Dot11(
+            proto=0,
+            addr1=dst_mac,
+            addr2=src_mac,
+            addr3=bssid,
+            SC=16,
+            ID=12288,
+            FCfield="to-DS+protected",
+            type="Data",
+            subtype="QoS Data",
+        )
+        / Dot11QoS(A_MSDU_Present=0, Ack_Policy=0, EOSP=0, TID=0, TXOP=0)
+        / Dot11CCMP(
+            PN0=pn[0], PN1=pn[1], PN2=pn[2], PN3=pn[3], PN4=pn[4], PN5=pn[5], ext_iv=1
+        )
+    )
 
 
 def gen_eapol_load(
@@ -81,7 +190,7 @@ def gen_eapol_load(
 
 def gen_pcaps(group_name):
     ssid = f"{group_name}_wifi"
-    passkey = math.pow(hash(f"{group_name}_password"), 2, 10**8)
+    passkey = str(pow(hash(f"{group_name}_password"), 2, 10**8))
     flag = hex(hash(group_name))[:16]
     packets = []
 
@@ -91,12 +200,12 @@ def gen_pcaps(group_name):
             type=0,
             subtype=2,
             addr1=bssid,
-            addr2=client_mac,
+            addr2=ALICE_MAC,
             addr3=bssid,
             FCfield="to-DS",
         )
         / Dot11ReassoReq(cap=0x1111, listen_interval=14)
-        / Dot11Elt(ID=0, info=ssid)
+        / Dot11Elt(ID=0, info=ALICE_MAC)
         / Dot11Elt(ID=1, info=b"\x82\x84\x8b\x96\x0c\x12\x18\x24")
         / Dot11Elt(ID=2, info=bssid)
         / Dot11Elt(
@@ -139,13 +248,18 @@ def gen_pcaps(group_name):
     replay_counter = b"\x00" * 7 + b"\x01"
     key_rsc = b"\x00" * 8
     key_id = b"\x00" * 8
-    key_mic = b"\xbb" * 16
+    dummy_key_mic = b"\x00" * 16
     key_descriptor_type = b"\x02"
     key_data_length = b"\x00" * 2
     key_data = b"\x00" * 1
     frame1 = (
         Dot11(
-            type=2, subtype=8, addr1=ssid, addr2=bssid, addr3=bssid, FCfield="from-DS"
+            type=2,
+            subtype=8,
+            addr1=ALICE_MAC,
+            addr2=bssid,
+            addr3=bssid,
+            FCfield="from-DS",
         )
         / Dot11QoS(TID=6)
         / LLC(dsap=0xAA, ssap=0xAA, ctrl=3)
@@ -161,17 +275,44 @@ def gen_pcaps(group_name):
                 key_iv,
                 key_rsc,
                 key_id,
-                key_mic,
+                dummy_key_mic,
+                # calc_mic(calc_ptk(calc_pmk(ssid.encode("utf-8"), passkey.encode("utf-8")), key_data), eapol_load),
                 key_data_length,
                 key_data,
             )
         )
     )
+    frame1_mic = calc_mic(
+        calc_ptk(calc_pmk(ssid.encode("utf-8"), passkey.encode("utf-8")), key_data),
+        frame1.build(),
+    )
+
+    frame1[Raw].load = gen_eapol_load(
+        key_descriptor_type,
+        key_info,
+        key_length,
+        replay_counter,
+        anonce,
+        key_iv,
+        key_rsc,
+        key_id,
+        frame1_mic,
+        key_data_length,
+        key_data,
+    )
+
     # Frame 2: Client to AP (SNonce)
     key_data = b"\x30\x14\x01\x00\x00\x0f\xac\x04\x01\x00\x00\x0f\xac\x04\x01\x00\x00\x0f\xac\x02\x0c\x00"
     key_info = b"\x01\x0a"
     frame2 = (
-        Dot11(type=2, subtype=8, addr1=bssid, addr2=ssid, addr3=bssid, FCfield="to-DS")
+        Dot11(
+            type=2,
+            subtype=8,
+            addr1=bssid,
+            addr2=ALICE_MAC,
+            addr3=bssid,
+            FCfield="to-DS",
+        )
         / Dot11QoS(TID=0, EOSP=1, TXOP=18)
         / LLC(dsap=0xAA, ssap=0xAA, ctrl=3)
         / SNAP(OUI=0, code=0x888E)
@@ -186,19 +327,40 @@ def gen_pcaps(group_name):
                 key_iv,
                 key_rsc,
                 key_id,
-                gen_mic(),
+                dummy_key_mic,
                 len(key_data).to_bytes(2, "big"),
                 key_data,
             )
         )
     )
-
+    frame2_mic = calc_mic(
+        calc_ptk(calc_pmk(ssid.encode("utf-8"), passkey.encode("utf-8")), key_data),
+        frame2.build(),
+    )
+    frame2[Raw].load = gen_eapol_load(
+        key_descriptor_type,
+        key_info,
+        key_length,
+        replay_counter,
+        snonce,
+        key_iv,
+        key_rsc,
+        key_id,
+        frame2_mic,
+        len(key_data).to_bytes(2, "big"),
+        key_data,
+    )
     # # Frame 3: AP to Client (MIC)
     key_info = b"\x13\xca"
     key_data = b"\xa0\x14\xaa\x8d\xa8\x3a\xeb\xfb\xb5\x9e\xed\x08\xf8\x9e\x7f\xd3\x37\x39\x7f\xef\x67\x3f\x9d\x41\xa8\x6f\x19\x42\xac\xbb\xe6\xd3\xf2\x6f\x0b\x66\x29\x37\xb6\x91\x9c\x0b\xb8\x38\xef\xfc\xce\x3e\x8b\xf6\xef\xf9\xaa\x08\x6b\x31"
     frame3 = (
         Dot11(
-            type=2, subtype=8, addr1=ssid, addr2=bssid, addr3=bssid, FCfield="from-DS"
+            type=2,
+            subtype=8,
+            addr1=ALICE_MAC,
+            addr2=bssid,
+            addr3=bssid,
+            FCfield="from-DS",
         )
         / Dot11QoS(TID=6)
         / LLC(dsap=0xAA, ssap=0xAA, ctrl=3)
@@ -214,18 +376,43 @@ def gen_pcaps(group_name):
                 key_iv,
                 key_rsc,
                 key_id,
-                gen_mic(),
+                dummy_key_mic,
                 len(key_data).to_bytes(2, "big"),
                 key_data,
             )
         )
+    )
+    frame3_mic = calc_mic(
+        calc_ptk(calc_pmk(ssid.encode("utf-8"), passkey.encode("utf-8")), key_data),
+        frame3.build(),
+    )
+
+    frame3[Raw].load = gen_eapol_load(
+        key_descriptor_type,
+        key_info,
+        key_length,
+        replay_counter,
+        anonce,
+        key_iv,
+        key_rsc,
+        key_id,
+        frame3_mic,
+        len(key_data).to_bytes(2, "big"),
+        key_data,
     )
 
     # # Frame 4: Client to AP (MIC)
     key_data = b"\x00" * 0
     key_info = b"\x03\x0a"
     frame4 = (
-        Dot11(type=2, subtype=8, addr1=bssid, addr2=ssid, addr3=bssid, FCfield="to-DS")
+        Dot11(
+            type=2,
+            subtype=8,
+            addr1=bssid,
+            addr2=ALICE_MAC,
+            addr3=bssid,
+            FCfield="to-DS",
+        )
         / Dot11QoS(TID=0, EOSP=1, TXOP=16)
         / LLC(dsap=0xAA, ssap=0xAA, ctrl=3)
         / SNAP(OUI=0, code=0x888E)
@@ -240,40 +427,59 @@ def gen_pcaps(group_name):
                 key_iv,
                 key_rsc,
                 key_id,
-                gen_mic(),
+                dummy_key_mic,
                 len(key_data).to_bytes(2, "big"),
                 key_data,
             )
         )
     )
+    frame4_mic = calc_mic(
+        calc_ptk(calc_pmk(ssid.encode("utf-8"), passkey.encode("utf-8")), key_data),
+        frame4.build(),
+    )
+
+    frame4[Raw].load = gen_eapol_load(
+        key_descriptor_type,
+        key_info,
+        key_length,
+        replay_counter,
+        b"\x00" * 32,
+        key_iv,
+        key_rsc,
+        key_id,
+        frame4_mic,
+        len(key_data).to_bytes(2, "big"),
+        key_data,
+    )
 
     initial_encrypted_packets = [
-        Ether(src=ALICE_MAC, dst=BOB_MAC)
-        / IP(src=ALICE_IP, dst=BOB_IP)
+        IP(src=ALICE_IP, dst=BOB_IP)
         / UDP(sport=12345, dport=54321)
         / Raw(
             load="Hey now that we're over an encrypted channel, can you tell me the flag?"
         ),
-        Ether(src=BOB_MAC, dst=ALICE_MAC)
-        / IP(src=BOB_IP, dst=ALICE_IP)
+        IP(src=BOB_IP, dst=ALICE_IP)
         / UDP(sport=54321, dport=12345)
         / Raw(load=f"Sure! The flag is: WPA2CTF\{{{flag}\}}"),
     ]
 
     packets += [reassocaiton_request, frame1, frame2, frame3, frame4]
-    packets += [
-        encrypt_packet(
-            anonce,
-            snonce,
-            ALICE_MAC,
-            BOB_MAC,
-            ssid,
-            passkey,
-            _packet.build(),
-            _packet[Dot11],
+    src_mac, dst_mac = ALICE_MAC, BOB_MAC
+    for i, _packet in enumerate(initial_encrypted_packets):
+        packets.append(
+            encrypt_packet(
+                src_mac,
+                dst_mac,
+                anonce,
+                snonce,
+                ssid,
+                passkey,
+                gen_header(i, src_mac, dst_mac, bssid),
+                _packet.build(),
+            )
         )
-        for _packet in initial_encrypted_packets
-    ]
+        src_mac, dst_mac = dst_mac, src_mac
+
     pcap_writer = PcapWriter(PCAP_LOC, linktype=105)
     for pkt in packets:
         pcap_writer.write(pkt.build())
@@ -281,4 +487,4 @@ def gen_pcaps(group_name):
 
 
 if __name__ == "__main__":
-    gen_pcap()
+    gen_pcaps("guacamole")
--- solution.py
+++ solution.py
@@ -6,6 +6,7 @@ import scapy.packet
 import sys
 from typing import *
 
+
 def get_handshake(packets: PacketList) -> List[scapy.packet]:
     handshake = []
     for packet in packets:
@@ -13,11 +14,14 @@ def get_handshake(packets: PacketList) -> List[scapy.packet]:
             handshake.append(packet)
     return handshake
 
+
 def mac_string_to_bytes(mac_string: str) -> bytes:
-    return binascii.unhexlify(mac_string.replace(":","", 5))
+    return binascii.unhexlify(mac_string.replace(":", "", 5))
+
 
 def get_eapol_bytes(packet: scapy.packet) -> bytes:
     return bytes(packet[EAPOL])
+
 
 def extract_nonces(handshake_data: List[bytes]) -> Tuple[bytes, bytes]:
     NONCE_OFFSET = 17
@@ -25,14 +29,16 @@ def extract_nonces(handshake_data: List[bytes]) -> Tuple[bytes, bytes]:
     NONCE_END = NONCE_OFFSET + NONCE_LENGTH
     anonce_packet_data = handshake_data[0]
     snonce_packet_data = handshake_data[1]
-    anonce = anonce_packet_data[NONCE_OFFSET : NONCE_END]
-    snonce = snonce_packet_data[NONCE_OFFSET : NONCE_END]
+    anonce = anonce_packet_data[NONCE_OFFSET:NONCE_END]
+    snonce = snonce_packet_data[NONCE_OFFSET:NONCE_END]
     return anonce, snonce
+
 
 def get_mic_frame(handshake_data: List[bytes]) -> bytes:
     mic_frame = handshake_data[1]
     mic_frame = mic_frame[:81] + b"\x00" * 16 + mic_frame[97:]
     return mic_frame
+
 
 def get_key_data(ap_mac: bytes, cl_mac: bytes, anonce: bytes, snonce: bytes) -> bytes:
     mac_addresses_pair = b"".join(sorted([ap_mac, cl_mac]))
@@ -40,17 +46,20 @@ def get_key_data(ap_mac: bytes, cl_mac: bytes, anonce: bytes, snonce: bytes) -> 
     key_data = mac_addresses_pair + nonce_pair
     return key_data
 
+
 def get_mic(handshake_data: List[bytes]) -> bytes:
     MIC_OFFSET = 81
     MIC_LENGTH = 16
     MIC_END = MIC_OFFSET + MIC_LENGTH
     mic_packet_data = handshake_data[1]
-    mic = mic_packet_data[MIC_OFFSET : MIC_END]
+    mic = mic_packet_data[MIC_OFFSET:MIC_END]
     return mic
 
+
 def calc_pmk(ssid: bytes, password: bytes) -> bytes:
-    pmk = hashlib.pbkdf2_hmac('sha1', password, ssid, 4096, 32)
+    pmk = hashlib.pbkdf2_hmac("sha1", password, ssid, 4096, 32)
     return pmk
+
 
 def calc_ptk(pmk: bytes, key_data: bytes) -> bytes:
     pke = b"Pairwise key expansion"
@@ -69,7 +78,23 @@ def calc_mic(ptk: bytes, mic_frame: bytes) -> bytes:
     mic = hmac.new(ptk[0:16], mic_frame, "sha1").digest()
     return mic[:-4]
 
-def crack_handshake(ap_ssid: str, handshake: List[scapy.packet], password_list, debug = False) -> None:
+
+def get_mic(ap_ssid, handshake):
+    handshake_data = [get_eapol_bytes(packet) for packet in handshake]
+    ap_mac = mac_string_to_bytes(handshake[0].addr2)
+    cl_mac = mac_string_to_bytes(handshake[0].addr1)
+    anonce, snonce = extract_nonces(handshake_data)
+    key_data = get_key_data(ap_mac, cl_mac, anonce, snonce)
+    mic_frame = get_mic_frame(handshake_data)
+
+    pmk = calc_pmk(ap_ssid.encode("ascii"), password.encode("ascii"))
+    ptk = calc_ptk(pmk, key_data)
+    return calc_mic(ptk, mic_frame)
+
+
+def crack_handshake(
+    ap_ssid: str, handshake: List[scapy.packet], password_list, debug=False
+) -> None:
     handshake_data = [get_eapol_bytes(packet) for packet in handshake]
     ap_mac = mac_string_to_bytes(handshake[0].addr2)
     cl_mac = mac_string_to_bytes(handshake[0].addr1)
@@ -79,7 +104,7 @@ def crack_handshake(ap_ssid: str, handshake: List[scapy.packet], password_list, 
     mic_frame = get_mic_frame(handshake_data)
 
     for password in password_list:
-        pmk = calc_pmk(ap_ssid.encode('ascii'), password.encode('ascii'))
+        pmk = calc_pmk(ap_ssid.encode("ascii"), password.encode("ascii"))
         ptk = calc_ptk(pmk, key_data)
         new_mic = calc_mic(ptk, mic_frame)
 
@@ -97,12 +122,14 @@ def crack_handshake(ap_ssid: str, handshake: List[scapy.packet], password_list, 
             print("[?] Calculated MIC : ", new_mic.hex())
             print("[?] Actual MIC     : ", mic.hex())
 
-def crack_eapol(ap_ssid: str, pcap_filename: str, password_list, debug = False):
+
+def crack_eapol(ap_ssid: str, pcap_filename: str, password_list, debug=False):
     packets = rdpcap(pcap_filename)
     crack_handshake(ap_ssid, get_handshake(packets), password_list, debug)
 
+
 def get_password_list(prefix: str):
-    
+
     def luhn_correct_bit(first_digits):
         res = 0
         i = 0
@@ -110,19 +137,21 @@ def get_password_list(prefix: str):
             val = int(dig)
             if i % 2 == 1:
                 if val >= 5:
-                    res += 2*val - 9
+                    res += 2 * val - 9
                 else:
-                    res += 2*val
+                    res += 2 * val
             else:
                 res += val
             i += 1
         return str((10 - res) % 10)
+
     remaining_digits = 8 - len(prefix)
-    for i in range(10 ** remaining_digits):
+    for i in range(10**remaining_digits):
         first_digits = prefix + str(i).zfill(remaining_digits)
         check_digit = luhn_correct_bit(first_digits)
         res = first_digits + check_digit
         yield res
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -132,5 +161,9 @@ if __name__ == "__main__":
     password_prefix = sys.argv[3]
     password_list = get_password_list(password_prefix)
     crack_eapol(ssid, filename, password_list)
-#crack_eapol("Building_G2", "/home/yuval/pcap/res.pcap-01.cap", ["not_password", "password", "123456789"])
-crack_eapol("Test_AP", "/home/yuval/Downloads/handshake-Test_AP.pcap", ["not_password", "password"])
+# crack_eapol("Building_G2", "/home/yuval/pcap/res.pcap-01.cap", ["not_password", "password", "123456789"])
+crack_eapol(
+    "Test_AP",
+    "/home/yuval/Downloads/handshake-Test_AP.pcap",
+    ["not_password", "password"],
+)
